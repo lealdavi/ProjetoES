2ada8ac8cc1ba2b9f031a76d231c3c5b089e8c73
letícia <lele.ramos.07@gmail.com>
2025-12-05 16:43:33 -0300

    Remove pasta do git

--- listarExerciciosService/exercicios.py
+++
@@ -1,6 +0,0 @@
-class Exercicio:
-    def __init__(self, id, nome, musculosTrabalhados, videoExecucao):
-        self.id = id
-        self.nome = nome
-        self.musculosTrabalhados = musculosTrabalhados
-        self.videoExecucao = videoExecucao
